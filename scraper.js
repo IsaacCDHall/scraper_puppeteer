f0540d7a6ebe761eabc5fb6f21537fc60ee371a7
Isaac Hall <isaacdehall@gmail.com>
2019-08-04 11:34:21 -0700

    scrape title data before pagination

--- scraper.js
+++ scraper.js
@@ -1,12 +1,18 @@
-
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.alibaba.com/catalog/cosmetics-packaging_cid231903?spm=a2700.details.scGlobalHomeHeader.512.5a9f6588y6UAEr')
-  await page.screenshot({path: 'example.png'})
+  await page.goto(
+    "https://www.alibaba.com/catalog/cosmetics-packaging_cid231903?spm=a2700.details.scGlobalHomeHeader.512.5a9f6588y6UAEr"
+  );
+  const screenshot = await page.screenshot({ path: "example.png" });
 
+  const titles = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("h2.title")).map(item =>
+      item.innerText.trim()
+    )
+  );
+  console.log(titles);
   await browser.close();
-
 })();
